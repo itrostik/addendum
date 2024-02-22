9d017a6358475b291c9c534ac751e168ce975ced
itrostik <RosASergeev@stud.kpfu.ru>
2024-02-22 11:35:04 +0300

    небольшие правки

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { ReactNode } from "react";
@@ -8,10 +7,10 @@ import "../styles/global.scss";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "PS Addendum",
-//   description: "Продажа/обмен игр PS5",
-// };
+export const metadata: Metadata = {
+  title: "PS Addendum",
+  description: "Продажа/обмен игр PS5",
+};
 
 export default function RootLayout({
   children,

8bcc14971bd396949b52c3e0e5a00d4b4470d138
itrostik <RosASergeev@stud.kpfu.ru>
2024-02-28 09:04:08 +0300

    внедрена модалка на главную страницу

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -6,10 +6,13 @@ import { Id } from "../../convex/_generated/dataModel";
 import Sidebar from "../Sidebar/Sidebar";
 import Loading from "../Loading/Loading";
 import Filter from "../Filter/Filter";
+import { Plus } from "lucide-react";
+import Modal from "../Modal/Modal";
 
 export default function Home() {
   const store = useMutation(api.users.store);
   const [userId, setUserId] = useState<Id<"users">>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   useEffect(() => {
     const getUser = async () => {
       setUserId(await store());
@@ -23,7 +26,25 @@ export default function Home() {
           <Sidebar user_id={userId} />
           <div className={styles.block}>
             <Filter />
+            <button
+              className={styles.addButton}
+              onClick={() => setIsOpen(true)}
+            >
+              <Plus className={styles.lucide} />
+            </button>
           </div>
+          {isOpen && (
+            <Modal setIsOpen={setIsOpen}>
+              <div className={styles.modalContent}>
+                <h1 className={styles.modalTitle}>Что хотите сделать?</h1>
+                <button className={styles.modalButton}>Продать</button>
+                <button className={styles.modalButton}>
+                  Выставить на аукцион
+                </button>
+                <button className={styles.modalButton}>Обменять</button>
+              </div>
+            </Modal>
+          )}
         </div>
       ) : (
         <Loading />

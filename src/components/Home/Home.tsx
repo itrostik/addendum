831594dfcc70874188236052c79cd8edcd714ef2
itrostik <RosASergeev@stud.kpfu.ru>
2024-03-26 15:49:00 +0300

    добавил компонент News

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,25 +2,22 @@
 import styles from "./Home.module.scss";
 import { useEffect, useState } from "react";
 import Sidebar from "@/components/Global/Sidebar/Sidebar";
-import Filter from "@/components/Global/Filter/Filter";
+import { Filter } from "@/components/Global/Filter/Filter";
 import { Plus } from "lucide-react";
 import Modal from "@/components/UI/Modal/Modal";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 import { useUser } from "@/hooks/useUser";
+import News from "@/components/News/News";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
   const user = useUser();
-
-  console.log(user);
-
   return (
     <>
       <div className={styles.wrapper}>
-        <Sidebar />
+        <Sidebar user={user} />
         <div className={styles.block}>
+          <News />
           <Filter />
           <button className={styles.addButton} onClick={() => setIsOpen(true)}>
             <Plus className={styles.lucide} />
